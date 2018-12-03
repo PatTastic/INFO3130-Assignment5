eb700cedd71dff17707498680c34d0d5ae1f02e7
Pat Wilken <patrickwilken28@gmail.com>
2018-12-02 19:25:12 -0500

    Added leaflet assets

---
+++ src/src/app/partials/view-suggestion/view-suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-suggestion',
+  templateUrl: './view-suggestion.component.html',
+  styleUrls: ['./view-suggestion.component.css']
+})
+export class ViewSuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
