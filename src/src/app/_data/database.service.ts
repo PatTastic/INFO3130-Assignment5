81029906a86858c85279bbd11cc8bfa8e44ddfdc
Pat Wilken <patrickwilken28@gmail.com>
2018-12-03 22:37:13 -0500

    Added database, more libraries
    libraries:
    - font awesome: regular
    - momentjs
    - angular material

--- src/src/app/_data/database.service.ts
+++ src/src/app/_data/database.service.ts
@@ -1,10 +1,66 @@
 ﻿import { Injectable } from '@angular/core';
 
+import { UtilitiesService } from '../_helpers/utilities.service';
+
 @Injectable()
 export class DatabaseService {
   db: any;
   
   constructor() {
     this.db = window['openDatabase']('info3130A5', '1.0', 'INFO3130 - A5', 2 * 1024 * 1024);
+    this.createDatabase();
+  }
+
+  private createDatabase() {
+    this.db.transaction((sqlTransactionSync) => {
+      sqlTransactionSync.executeSql(
+        'CREATE TABLE IF NOT EXISTS geoPoint(' +
+        'id INTEGER PRIMARY KEY UNIQUE, ' +
+        'lat REAL NOT NULL, ' +
+        'lng REAL NOT NULL, ' +
+        'address TEXT, ' +
+        'ts INTEGER NOT NULL, ' +
+        'day TEXT NOT NULL, ' +
+        'weight INTEGER NOT NULL)',
+        []
+      );
+
+      sqlTransactionSync.executeSql(
+        'CREATE TABLE IF NOT EXISTS analytics(' +
+        'id INTEGER PRIMARY KEY UNIQUE, ' +
+        'type TEXT NOT NULL, ' +
+        'averageCost REAL, ' +
+        'averageRating REAL)',
+        []
+      );
+    }, (msg) => {
+      console.log('SQL: createDatabase', msg);
+    });
+  }
+
+  getActiveDays() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.db.transaction((sqlTransactionSync) => {
+          let res: any = sqlTransactionSync.executeSql('SELECT DISTINCT day FROM geoPoint', []);
+
+          if (!UtilitiesService.doesExist(res)) {
+            res = [];
+          }
+
+          for (let i = 0; i < res.length; i++) {
+            res[i] = UtilitiesService.convertDateToUniformDate(res[i]);
+          }
+          res.push(UtilitiesService.convertDateToUniformDate(new Date().toString()));
+
+          resolve(res);
+        }, (msg) => {
+          console.log('SQL: getActiveDays', msg);
+        });
+      }
+      catch (err) {
+        reject(false);
+      }
+    })
   }
 }
--- src/src/app/_helpers/utilities.service.ts
+++ src/src/app/_helpers/utilities.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable()
 export class UtilitiesService {
+  static deepCopy(elem: any) {
+    return JSON.parse(JSON.stringify(elem));
+  }
 
-  constructor() { }
+  static doesExist(elem: any) {
+    return !(typeof elem == 'undefined' || elem == null);
+  }
 
+  static convertDateToUniformDate(date: string) {
+    return moment(new Date(date)).format('DD-MM-YYYY');
+  }
 }
