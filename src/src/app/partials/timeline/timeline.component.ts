45112bc549c208a4e9567d0ffe57f4e927852ea4
Pat Wilken <patrickwilken28@gmail.com>
2018-12-03 22:37:33 -0500

    Added date picker to timeline

--- src/src/app/partials/timeline/timeline.component.ts
+++ src/src/app/partials/timeline/timeline.component.ts
@@ -1,7 +1,10 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import * as moment from 'moment';
 
 import { ConfigService } from '../../_helpers/config.service';
+import { DatabaseService } from '../../_data/database.service';
+import { UtilitiesService } from '../../_helpers/utilities.service';
 
 @Component({
   selector: 'app-timeline',
@@ -9,14 +12,66 @@ import { ConfigService } from '../../_helpers/config.service';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
-  options: any;
+  private activeDays: string[];
 
-  constructor() {
+  options: any;
+  selectedDay: any;
+  showCalendar: boolean;
+
+  constructor(private _db: DatabaseService) {
+    this.showCalendar = false;
     this.options = ConfigService.getDefaultMapOptions();
     this.options.loaded = true;
+
+    this.selectedDay = {
+      date: moment(new Date()).format('MMMM Do, YYYY'),
+      dateMsg: ''
+    };
   }
 
   ngOnInit() {
+    this._db.getActiveDays().then((res) => {
+      this.activeDays = UtilitiesService.deepCopy(res);
+    })
+  }
+  
+  onCalendarSelectedChange(date: string) {
+    this.selectedDay.date = moment(new Date(date)).format('MMMM Do, YYYY');
+    let formattedDate = UtilitiesService.convertDateToUniformDate(date);
+    let isActiveDay = false;
+
+    for (let i = 0; i < this.activeDays.length; i++) {
+      if (this.activeDays[i] == formattedDate) {
+        isActiveDay = true;
+        break;
+      }
+    }
+
+    if (isActiveDay) {
+      this.selectedDay.dateMsg = '';
+      this.toggleCalendar({ target: { nodeName: 'svg' } });
+      //this.loadDay(formattedDate);
+    }
+    else {
+      this.selectedDay.dateMsg = 'No data for selected date';
+      setTimeout(() => {
+        this.selectedDay.dateMsg = '';
+      }, 3000)
+    }
   }
 
+  toggleCalendar(event: any) {
+    let toggle = false;
+
+    if (event.target.nodeName == 'path' || event.target.nodeName == 'svg') {
+      toggle = true;
+    }
+    else if (event.target.className == 'calendar-container' || event.target.className.indexOf('calendar-icon') > -1) {
+      toggle = true;
+    }
+
+    if (toggle) {
+        this.showCalendar = !this.showCalendar;
+      }
+    }
 }
