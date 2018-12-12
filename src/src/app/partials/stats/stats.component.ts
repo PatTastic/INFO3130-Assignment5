f4efd04deb2bc508f8fb62236fe47dde9bd0b5ca
Pat Wilken <patrickwilken28@gmail.com>
2018-12-11 19:27:32 -0500

    Made distance chart dynamic

--- src/src/app/partials/stats/stats.component.ts
+++ src/src/app/partials/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { UtilitiesService } from './../../_helpers/utilities.service';
 import { ApiService } from './../../_services/api.service';
@@ -17,7 +17,7 @@ export class StatsComponent implements OnInit {
   locationInfo = [];
   distanceInfo = [];
   locationLabels = [];
-  distanceLabels = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+  distanceLabels = [];
   locationChartDisplay = true;
   distanceChartDisplay = true;
 
@@ -26,45 +26,44 @@ export class StatsComponent implements OnInit {
       // type, count
       const objectLength = Object.keys(locationData).length;
       if(objectLength > 0) {
-          for(let i = 0; i < objectLength; i++) {
-            this.locationInfo.push(locationData[i].count);
-            this.locationLabels.push(locationData[i].type);    
-            if(objectLength == this.locationInfo.length) {
-              this.generateLocationChart();
-            }          
-          }
-      } else {
-          this.locationChartDisplay = false;
-      }
-
-    });
-    this._api.getTestData().then((distanceData) => {
-      const objectLength = Object.keys(distanceData).length;
-
-      if (objectLength > 0) {
-        let dist = 0;
         for (let i = 0; i < objectLength; i++) {
-          if (i < objectLength - 1) {
-            if (distanceData[i].day === distanceData[i + 1].day) {
-              var start = { latitude: distanceData[i].lat, longitude: distanceData[i].lng };
-              var end = { latitude: distanceData[i + 1].lat, longitude: distanceData[i + 1].lng };
-              dist += UtilitiesService.distance(start, end);
-
-            } else {
-              this.distanceInfo.push(dist * 0.001);
-              dist = 0;
-            }
-          } else {
-            this.generateDistanceChart();
-          }
+          locationData[i].type = (UtilitiesService.toTitleCase(locationData[i].type));
+          this.locationInfo.push(locationData[i].count);
+          this.locationLabels.push(locationData[i].type);    
+          if(objectLength == this.locationInfo.length) {
+            this.generateLocationChart();
+          }          
         }
-      } else {
-        this.distanceChartDisplay = false;
+      }
+      else {
+        this.locationChartDisplay = false;
       }
     });
+
+    this._api.getActiveDays().then((res: any) => {
+      if (res !== false) {
+        let length = (res.length < 5 ? res.length : 5);
+        for (let i = 0; i < length; i++) {
+          this.distanceLabels.push(res[i]);
+
+          this._api.getDay(res[i]).then((days: any) => {
+            if (days.length > 1) {
+              for (let j = 0; j < (days.length - 1); j++) {
+                let distance = UtilitiesService.distance(days[j], days[j + 1]);
+                this.distanceInfo.push(distance);
+              }
+            }
+
+            if (i == (length - 1)) {
+              this.generateDistanceChart(length);
+            }
+          })
+        }
+      }
+    })
   }
 
-  generateDistanceChart() {
+  generateDistanceChart(length: number) {
     this.locationChart = new Chart('distanceCanvas', {
       type: 'line',
       data: {
@@ -72,7 +71,7 @@ export class StatsComponent implements OnInit {
         datasets: [
           {
             data: this.distanceInfo,
-            borderColor: "#ffcc00",
+            borderColor: "#BB9046",
             fill: false
           },
         ]
@@ -85,14 +84,14 @@ export class StatsComponent implements OnInit {
           xAxes: [{
             scaleLabel:{
               display: true,
-							labelString: 'Weeks'
+              labelString: 'Last ' + length.toString() + ' Active Days'
             },
             display: true
           }],
           yAxes: [{
             scaleLabel:{
               display: true,
-							labelString: 'Distance(km)'
+							labelString: 'Distance (m)'
             },
             display: true
           }],
@@ -109,7 +108,7 @@ export class StatsComponent implements OnInit {
         datasets: [
           {
             data: this.locationInfo,
-            backgroundColor: "#ffcc00"
+            backgroundColor: "#BB9046"
           },
         ]
       },
@@ -121,7 +120,7 @@ export class StatsComponent implements OnInit {
           xAxes: [{
             scaleLabel:{
               display: true,
-							labelString: 'Locations'
+							labelString: 'Location Types'
             },
             display: true
           }],
